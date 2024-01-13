2d7f8a9e0a3a425c9e6a19bd34700132764f3da3
Brandon Lim <limws.brandon@gmail.com>
2024-01-13 23:48:13 +0800

    Add comments to propose code changes

--- RadioactivityMonitor/Alarm.cs
+++ RadioactivityMonitor/Alarm.cs
@@ -18,10 +18,17 @@ namespace RadioactivityMonitor
         public void Check()
         {
             double value = SensorMeasurement.Invoke();
-
+            /*
+             * The if statement can be inverted and the function returned early.
+             * This makes the code cleaner and easier to read.
+             */
             if (value < LowThreshold | HighThreshold < value)
             {
                 _alarmOn = true;
+                /*
+                 * _alarmCount can be removed if it is not used anywhere.
+                 * Additional properties add an overhead to code maintenance.
+                 */
                 _alarmCount += 1;
             }
         }
