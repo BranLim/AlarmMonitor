d02e246841cee7f6dc29160a05c18d8a33de5b1e
Brandon Lim <laydon.lin6021@gmail.com>
2024-01-15 14:52:54 +0800

    Add readme. Update tests. Set .NET version

---
+++ README.md
@@ -0,0 +1,6 @@
+# Overview
+
+This project is created using JetBrains Rider and configured to run on .NET 8.
+
+
+
--- RadioactivityMonitorTests/AlarmTest.cs
+++ RadioactivityMonitorTests/AlarmTest.cs
@@ -4,69 +4,80 @@ namespace RadioactivityMonitorTests;
 
 public class AlarmTest
 {
-
     [Test]
     public void GivenMeasurementBelowThreshold_ThenAlarmIsSet()
     {
-        var alarm = new Alarm
-        {
-            SensorMeasurement = () => Alarm.LowThreshold - 1
-        };
+        var alarm = CreateAlarmWithSensorBelowThreshold();
 
         alarm.Check();
 
         Assert.IsTrue(alarm.AlarmOn, "Alarm should be true but received false");
     }
-    
+
+
     [Test]
     public void GivenMeasurementAboveThreshold_ThenAlarmIsSet()
     {
-        var alarm = new Alarm
-        {
-            SensorMeasurement = () => Alarm.HighThreshold + 1
-        };
+        var alarm = CreateAlarmWithSensorAboveThreshold();
 
         alarm.Check();
 
         Assert.IsTrue(alarm.AlarmOn, "Alarm should be true but received false");
     }
-    
+
+
     [Test]
     public void GivenMeasurementIsWithinThreshold_ThenAlarmIsNotSet()
     {
-        var alarm = new Alarm
-        {
-            SensorMeasurement = () => Alarm.HighThreshold - 1
-        };
+        var alarm = CreateAlarmWithSensorWithinThreshold();
 
         alarm.Check();
 
         Assert.IsFalse(alarm.AlarmOn, "Alarm should be false but received true");
     }
-    
+
     [Test]
     public void GivenMeasurementIsHigherThanThreshold_ThenAlarmCountIs1()
     {
-        var alarm = new Alarm
-        {
-            SensorMeasurement = () => Alarm.HighThreshold + 1
-        };
+        var alarm = CreateAlarmWithSensorAboveThreshold();
 
         alarm.Check();
 
         Assert.That(alarm.AlarmCount, Is.EqualTo(1));
     }
-    
+
     [Test]
     public void GivenMeasurementIsLowerThanThreshold_ThenAlarmCountIs1()
     {
-        var alarm = new Alarm
-        {
-            SensorMeasurement = () => Alarm.LowThreshold - 1
-        };
+        var alarm = CreateAlarmWithSensorBelowThreshold();
 
         alarm.Check();
 
         Assert.That(alarm.AlarmCount, Is.EqualTo(1));
     }
+
+    [Test]
+    public void GivenMeasurementIsWithinThreshold_ThenAlarmCountIs0()
+    {
+        var alarm = CreateAlarmWithSensorWithinThreshold();
+
+        alarm.Check();
+
+        Assert.That(alarm.AlarmCount, Is.EqualTo(0));
+    }
+
+    private static Alarm CreateAlarmWithSensorAboveThreshold() => new Alarm
+    {
+        SensorMeasurement = () => Alarm.HighThreshold + 1
+    };
+
+    private static Alarm CreateAlarmWithSensorBelowThreshold() => new Alarm
+    {
+        SensorMeasurement = () => Alarm.LowThreshold - 1
+    };
+
+    private static Alarm CreateAlarmWithSensorWithinThreshold() => new Alarm
+    {
+        SensorMeasurement = () => Alarm.LowThreshold + 1
+    };
 }
